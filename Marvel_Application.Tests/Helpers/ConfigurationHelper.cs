cd09a2eb225c5715d50d07c7c776c1bb70945cf9
Juan Salazar <juan.salazar4@soy.sena.edu.co>
2024-09-23 14:11:27 -0500

    Added Unit Tests

---
+++ Marvel_Application.Tests/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace Marvel_Application.Tests.Helpers
+{
+    public static class ConfigurationHelper
+    {
+        public static void SetAppSettings(string key, string value)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings[key] != null)
+            {
+                config.AppSettings.Settings[key].Value = value;
+            }
+            else
+            {
+                config.AppSettings.Settings.Add(key, value);
+            }
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
