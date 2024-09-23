7ee115b98dc7754d0a2d2f4d58c46a8aa6f2c359
Juan Salazar <juan.salazar4@soy.sena.edu.co>
2024-09-23 12:32:32 -0500

    Fixed problems with nested files

--- Marvel_Application/src/Views/Characters.aspx.cs
+++ Marvel_Application/src/Views/Characters.aspx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
-namespace Marvel_Application.Views
+namespace Marvel_Application.src.Views
 {
-    public partial class Characters : Page
+    public partial class Characters : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
